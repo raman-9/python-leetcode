fa4c86e4d26ef347396d5c726aabebcf9ccc5911
Raman kumar <ramanpandit2028@gmail.com>
2026-02-05 22:37:30 +0530

    add coments

--- sorted_array.py
+++ sorted_array.py
@@ -1,8 +1,8 @@
 def findMedianSortedArrays(self,num1,num2):
     merged=[]
-    i=j=0 #pointer of num1 and num2
+    i=j=0 #pointer of num1 and num2 with complexity O(log(m+n))
     
-    while i<len(num1) and i<len(num2):
+    while i<len(num1) and i<len(num2): 
         if num1[i]<num2[j]:
             merged.append(num1[i])
             i+=1
